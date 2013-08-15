27f270e95034ef5c2e8d4f226090962920ab2175
terry-chelsea <fengyuatad@126.com>
2013-08-15 15:56:30 +0800

    init add directorys...

---
+++ timer/timer/timer_client.c
@@ -0,0 +1,15 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  timer_client.c
+ *
+ *    Description:  client of timer client , use named pipe send mssage ...
+ *
+ *        Version:  1.0
+ *        Created:  07/20/13 13:26:06
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ * =====================================================================================
+ */
+
